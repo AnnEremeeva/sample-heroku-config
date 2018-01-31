08de8731e18e9253c66f91df2f215fd01b883b2a
AnnEremeeva <eremeeva.anny@gmail.com>
2018-01-31 17:21:49 +0300

    Update handler.py

--- handler.py
+++ handler.py
@@ -1,20 +1,23 @@
 #import requests
 #import json
+#import emotions
 URL = "https://api.telegram.org/bot497831974:AAGQf1mIsQlgIrFSFOH09i5g-mynyAfHlVc/getUpdates"
 
 def handle_message(message, nickname="user"):
-    return()
+    if message == 'ПОЗИТИВ!':
+        return'igbbhreb'
+    elif message == 'БОЛЬ!':
+        return'gfbnrthw'
+    else:
+        return 'bheobjrjegh'
 
 if __name__ == "__main__":
     # dirty python magic, will talk about on the next lesson
     # just ignore for now
 
-    nick = input("Enter your nickname: ")
+    nick = input("Как вас называть?")
 
     while True:
-        msg = input("Your message: ")
+        msg = input("Если вы хотите позитивный смайлик, то напишите: 'ПОЗИТИВ!'. Если вы хотите отрицательный смайлик, то напишите: 'БОЛЬ!'")
         ans = handle_message(msg, nick)
-        if msg[:5] == "Что в":
-            print(*ans, sep = ', ')
-        else:
-            print(ans)
+        print(ans)
